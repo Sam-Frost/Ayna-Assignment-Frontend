28defccb3eb043ba9622d595b065bcf66e2a9091
Sam-Frost <sambrofrost@gmail.com>
2024-07-06 13:26:40 +0530

    Fixed web socket url

--- src/socket.ts
+++ src/socket.ts
@@ -2,11 +2,10 @@ import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
 // const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4000';
-var URL = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
+const URL = process.env.NEXT_PUBLIC_WEBSOCKET_URL ?? "";
 
-URL.replace(/^http/, 'ws')
 
 
 export const socket = io(URL, {
-    autoConnect: true
+    autoConnect: false
   });
